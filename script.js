5fcee5818d919d0f2e74252ef4d1c58e93558b22
gitsairam <sairamvenkata194@gmail.com>
2022-04-05 17:58:38 +0530

    1 file modified

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const input = document.querySelector('input');
 const defaultSpan = document.getElementById('default');
 const debounceSpan = document.getElementById('debounce');
 const throttleSpan = document.getElementById('throttle');
+
+input.addEventListener('input', (e) => {
+  defaultSpan.textContent = e.target.value;
+  updateDebounceText(e.target.value);
+});
+
+const updateDebounceText = debounce((text) => {
+  debounceSpan.textContent = text;
+});
+
+function debounce(cb, delay = 1000) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
